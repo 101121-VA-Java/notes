fe6c8f20686019cc60252fc469a4106f3a897c33
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-11-10 10:17:42 -0500

    adding example html

---
+++ week 5/js-practice/exercise.js
@@ -0,0 +1,30 @@
+// Problem 1: Create a function "isWeekday" which takes a single argument and returns true if the argument is a day during the work week. It should return false if it is not.
+
+function isWeekday(input) {
+    return;
+}
+
+// Problem 2: Create a function "isEven" which takes a single argument and returns true if the argument is an even integer. It should return false if it is not
+
+function isEven(input) {
+    return;
+}
+
+// Problem 3: Write a JavaScript function which takes a single argument and returns the type
+
+function findType(input) {
+    return;
+}
+
+// Problem 4: Write a JavaScript function which takes a single argument and determines if it's prime. If it is, return true. If it's not return false.
+
+function isPrime(input) {
+
+    return;
+}
+
+// Problem 5: Write a JavaScript function which calculates the first number to the power of the second number. Do not do this recursively. Do not use the Math library.
+
+function calculateExponent(base, exponent) {
+    return;
+}
