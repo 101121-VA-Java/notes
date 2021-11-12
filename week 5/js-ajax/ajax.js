11719eb358c8f40b3fb6d5a18e0e51a79c96d710
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-11-12 14:41:18 -0500

    adding ajax

--- week 5/js-ajax/ajax.js
+++ week 5/js-ajax/ajax.js
@@ -24,6 +24,7 @@ function getData() {
         // what to be done once the data is ready
         if (xhr.readyState === 4) {
             let dataSpan = document.getElementById('data');
+             // Resets the innerHTML for every request
             dataSpan.innerHTML = '';
             if (xhr.status >= 200 && xhr.status < 300) {
                 let response = xhr.response;
@@ -45,4 +46,34 @@ function populateData(response) {
     // console.log(response);
     console.log(response.name);
     // Using DOM manipulation, display information about pokemon to the webpage
+
+    let dataSection = document.getElementById('data');
+
+   let nameTag = document.createElement('h3');
+   nameTag.innerHTML = response.name.toUpperCase();
+   
+   let abilitiesArray = response.abilities;
+   let abilities = document.createElement('ul');
+   // Appending list elements to a ul
+   // for of loops return the object itself
+   for(let ability of abilitiesArray){
+       let abilityLi = document.createElement('li');
+       abilityLi.innerHTML = ability.ability.name;
+       abilities.appendChild(abilityLi);
+   }
+
+   // Appending h3 and List to the section tag
+   dataSection.appendChild(nameTag);
+   dataSection.appendChild(abilities);
+
+   // Appending sprites to section
+   let spritesObject = response.sprites;
+   // for in loops return a list of keys of the object iterated over
+   for(let key in spritesObject){
+       if(spritesObject[key] && spritesObject[key].length > 2){
+           let spriteImg = document.createElement('img');
+           spriteImg.src = spritesObject[key];
+           dataSection.appendChild(spriteImg);
+       }
+   }
 }
