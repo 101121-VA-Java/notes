f9b2866d431720b48bacc4084da91cfa0420ef06
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-11-29 09:56:38 -0500

    adding monday plan

--- week 6/base-app/src/main/java/com/revature/Driver.java
+++ week 6/base-app/src/main/java/com/revature/Driver.java
@@ -13,7 +13,6 @@ import io.javalin.Javalin;
 public class Driver {
 
 	public static void main(String[] args) {
-
 		// Creating an instance of javalin and starting on port 8080
 		Javalin app = Javalin.create((config) -> {
 			config.enableCorsForAllOrigins();
